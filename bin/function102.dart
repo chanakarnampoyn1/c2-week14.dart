e19b7b9b140cfcaa9e2f17f5fbc115463b670a8a
chanakarnampoyn1 <bidseesee89@gmail.com>
2022-08-30 14:58:05 +0700

    first commit

---
+++ bin/function102.dart
@@ -0,0 +1,10 @@
+void main(){
+  message("Mark","27/8","06512410");
+}
+
+void message(String name, String address, String phone){
+
+  print("Yo0r name is $name");
+  print("You address is $address");
+  print("You phone number is $phone");
+}
